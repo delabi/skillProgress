4b8cc516559231c8c142d0bf53f1e8c363ece20b
redlion47 <katoo.eric@gmail.com>
2017-04-07 16:42:46 +0300

    debugged code

--- change_from_not_done_to_done.py
+++ change_from_not_done_to_done.py
@@ -1,45 +1,48 @@
 
-def remove_from_not_done_list(notdonelist, selected):#function that removes the item done from not done list
+def remove_from_not_done_list(arr1, num1):#function that removes the item done from not done list
 
-	del(notdonelist[selected])#remove the selected item from the list of not done
+	del(arr1[num1])#remove the selected item from the list of not done
 
-	new_notdonelist = notdonelist#create a new list of the remaining items
+	new_arr = arr1#create a new list of the remaining items
 
-	return new_notdonelist
+	return new_arr
 
-def from_not_done_to_done(notdonelist, selected, donelist):#function that updates the removed item from not done list into done list
-	key = selected
+def from_not_done_to_done(array1, number, array2):#function that updates the removed item from not done list into done list
+	#print(number)
+	#print(array1)
+	#print(array2)
 
-	selectedvalue = notdonelist[key]#find the selected item
+	selectedvalue = array1[number]#find the selected item
 
-	donelist[selected] = selectedvalue #new list of done items
+	array2[number] = selectedvalue #new list of done items
 
-	done_items = donelist
+	done_arr = array2
 
-	return done_items
+	return done_arr
 
 def progress(notdonelist, donelist):#gives out the progress of the user
 
-	lengthofnotdone = len(new_notdonelist)#number of remaining items
+	lengthofnotdone = len(notdonelist)#number of remaining items
 
-	lengthofdoneitems = len(done_items)
+	lengthofdoneitems = len(donelist)
 
 	progress = ((lengthofdoneitems*100)//(lengthofdoneitems + lengthofnotdone))
 
 	return progress
 
 
-notdonelist = {1: 'a', 2: 'e', 3: 'i', 4: 'o', 5:'u'}
-selected1 = 4
+notdone = {1: 'a', 2: 'e', 3: 'i', 4: 'o', 5:'u'}
+chosen = 2
+listdone = {7:'t',5:'k'}
+doneitems = from_not_done_to_done(notdone, chosen, listdone)
 
-notdonelist1 = remove_from_not_done_list(notdonelist,selected1)
+print(doneitems)
+notdonelist1 = remove_from_not_done_list(notdone,chosen)
 
 print(notdonelist1)
 
-doneitems = from_not_done_to_done(notdonelist,selected1,{7:'t'})
 
-print(doneitems)
 
-progressobj = progress(notdonelist1, doneitems)
+#progressobj = progress(notdone, {7:'t',5:'k'})
 
-print(progressobj)
+#print(progressobj)
